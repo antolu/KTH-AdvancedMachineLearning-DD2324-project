ca825ba4a24cfaaab2bbf9dfdadf03db6f1c4723
gustavkjellberg <gustav.kjellberg@hotmail.com>
2019-01-03 17:54:38 +0100

    Created basic structure

--- SSK_Kernel.py
+++ SSK_Kernel.py
@@ -1,5 +1,14 @@
 """ Reimplementation of the SSK kernel described in Text Classification using String Kernels by Lodhi et al."""
 
+class SSK():
+    """ SSK class to handle all properites of the SSK kernel"""
+
+    def __init__(self, n, l, features, s, t):
+        self.n = n
+        self.l = l
+        self.features = features
+        self.s = s
+        self.t = t
 
 
 
@@ -9,5 +18,9 @@
 
 
 
-if if __name__ == "__main__":
-    pass
+
+if __name__ == "__main__":
+    s = "hej"
+    t = "he"
+    
+    ssk = SSK(3, 0.3, 10, s, t)
