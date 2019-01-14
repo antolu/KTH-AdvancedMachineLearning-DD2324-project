cb91b1fcb9445f15e35d4a0d3f039eed3439e9de
Anton Lu <anton.lu@epfl.ch>
2019-01-14 11:40:14 +0100

    Fix things

--- kernels.py
+++ kernels.py
@@ -4,7 +4,7 @@ from collections import Counter
 from scipy.sparse import csr_matrix
 from nltk.tokenize import word_tokenize
 from ssk_ap import SSK_AP, get_features, count_occurrences
-from ssk import SSK
+from SSK_Kernel import SSK
 import re
 
 regex = re.compile(r"\s")
@@ -120,7 +120,8 @@ def compute_matrix(documents, kernel="ngk", n=2, x=100, l=0.2) :
     if kernel == "ngk" :
         kernel = ngk
     elif kernel == "ssk" :
-        kernel = ngk
+        ssk = SSK(n, l, x, "", "")
+        kernel = ssk.k
     elif kernel == "wk" :
         kernel = wk
     elif kernel == "ssk_ap" :
@@ -150,8 +151,8 @@ def compute_matrix(documents, kernel="ngk", n=2, x=100, l=0.2) :
             kernel_matrix[n2, n1] = val
 
     # Normalize
-    for n1 in range(N) : 
-        for n2 in range(N) :
-            kernel_matrix[n1, n2] = kernel_matrix[n1, n2] / m.sqrt(kernel_matrix[n1, n1] * kernel_matrix[n2, n2])
+    # for n1 in range(N) : 
+    #     for n2 in range(N) :
+    #         kernel_matrix[n1, n2] = kernel_matrix[n1, n2] / m.sqrt(kernel_matrix[n1, n1] * kernel_matrix[n2, n2])
 
     return kernel_matrix
