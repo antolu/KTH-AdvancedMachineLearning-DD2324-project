c2f2f6e81d48b62a57666bf438251f80d52d6e08
Anton Lu <anton.lu@epfl.ch>
2019-01-18 14:40:22 +0100

    Fix header mismatch

--- ssk.hpp
+++ ssk.hpp
@@ -5,9 +5,9 @@
 #include <cmath>
 #include <boost/python.hpp>
 
-double kb(std::string s, std::string t, const double & l, const int n);
+double kb(std::string s, std::string t, const double & l, const int & n);
 
-double kp(std::string, std::string t, const double & l, const int n);
+double kp(std::string, std::string t, const double & l, const int & n);
 
 double ssk(const std::string s, const std::string t, const double & l, const int & n);
 
