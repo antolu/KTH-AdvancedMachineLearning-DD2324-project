dc7677419503a3e74b8f01d74e819d4b7d125515
hhaggren <hhaggren@users.noreply.gits-15.sys.kth.se>
2019-01-19 15:08:40 +0100

    Added class labels to score output in svm.py

--- svm.py
+++ svm.py
@@ -1,9 +1,10 @@
 from sklearn.svm import SVC,LinearSVC
+from sklearn.neural_network import MLPClassifier
 from sklearn import preprocessing
 #import svmlight
 import sklearn.metrics as skmet
 import numpy as np
-
+from constants import CATEGORIES
 
 
 class TextClassifier:
@@ -29,37 +30,56 @@ class TextClassifier:
         if ((ker_type == "ssk") or (ker_type == "ngk")):
             clf = SVC(kernel = "precomputed")
         elif (ker_type == "wk"):
-            clf = LinearSVC(kernel="precomputed")
+            clf = SVC(kernel="precomputed")
         else:
             print("No valid kernel defined.")
 
-        #self.transform_labels()
-        print(self.test_matrix.shape)
+
+        clf.fit(self.train_matrix, self.train_labels)
+        y_pred = clf.predict(self.test_matrix)
+        accuracy = clf.score(self.test_matrix, self.test_labels)
+
+        precision,recall,f1score,supp = skmet.precision_recall_fscore_support(y_true=self.test_labels, y_pred=y_pred, labels=CATEGORIES)
+
+        return precision,recall,f1score
+
+
+    def ANN(self):
+        clf = MLPClassifier(solver="lbfgs")
+
         clf.fit(self.train_matrix, self.train_labels)
         y_pred = clf.predict(self.test_matrix)
 
+        precision, recall, f1score, supp = skmet.precision_recall_fscore_support(y_true=self.test_labels, y_pred=y_pred,
+                                                                                 labels=CATEGORIES)
 
-        precision,recall,f1,supp = skmet.precision_recall_fscore_support(y_true=self.test_labels, y_pred=y_pred)
-
-        return f1,precision,recall
-
+        return precision, recall, f1score
 
 
 '''
 Loads relevant test/train-kernel matrices and their respective class labels
 '''
 
-test = np.load("results/ngk_testmatrix.npy")
-train = np.load("results/ngk_trainmatrix.npy")
-test_labels = np.load("results/ngk_testclasses.npy")
-train_labels = np.load("results/ngk_trainclasses.npy")
+test = np.load("results/sskap_testmatrix.npy")
+train = np.load("results/sskap_trainmatrix.npy")
+test_labels_byte = np.load("results/sskap_testclasses.npy")
+train_labels_byte = np.load("results/sskap_trainclasses.npy")
+train_labels = []
+test_labels = []
+
+for item in train_labels_byte:
+    train_labels.append(item.decode('UTF-8'))
+
+for item in test_labels_byte:
+    test_labels.append(item.decode('UTF-8'))
+
 
 
 classifier = TextClassifier(train, test, train_labels, test_labels)
 
-f1_out,prec_out,recall_out = classifier.SVM(ker_type="ngk")
-
-
-print(f1_out)
-print(prec_out)
-print(recall_out)
+prec,rec,f1 = classifier.SVM("ssk")
+#prec,rec,f1 = classifier.ANN()
+print("Category: ", CATEGORIES)
+print("F1: ",f1)
+print("Prec: ",prec)
+print("Recall: ",rec)
